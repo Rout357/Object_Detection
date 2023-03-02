4b5196f5eb547c7a733077bc781809932f4cadc2
Ruslan Muzyra <rmuzyra20@gmail.com>
2023-03-03 00:09:48 +0200

    change path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pixellib.instance import instance_segmentation
 #Function that detected object
 def object_detection():
     segment_image = instance_segmentation()
-    segment_image.load_model("C:\\Users\\Ruslan\\Desktop\\Python knowleges\\MyProjects\\GitHub\\Object_Detection\\mask_rcnn_coco.h5")
+    segment_image.load_model("path to model -> mask_rcnn_coco.h5")
     
     #Pass the argument of the thing we want to recognize
     target_class = segment_image.select_target_classes(car=True)
